1cd8e6ce98e51c22e7974a21aefe7f3565e10c65
Yaduveer Singh <yaduveers02@gmail.com>
2023-12-27 21:11:09 +0530

    new question

--- Graph/src/graph/Main.java
+++ Graph/src/graph/Main.java
@@ -3,14 +3,27 @@ package graph;
 import java.util.ArrayList;
 
 public class Main {
-	
+
+	static class Edge {
+
+		int src;
+		int nbr;
+		int wt;
+
+		Edge(int src, int nbr, int wt) {
+			this.nbr = nbr;
+			this.src = src;
+			this.wt = wt;
+		}
+	}
+
 	public static void main(String[] args) {
-		int vtes =7;
+		int vtes = 7;
 		ArrayList<Edge>[] graph = new ArrayList[vtes];
-		for(int i=0; i<vtes; i++) {
+		for (int i = 0; i < vtes; i++) {
 			graph[i] = new ArrayList<Edge>();
 		}
-		
+
 		graph[0].add(new Edge(0, 1, 10));
 		graph[0].add(new Edge(0, 3, 40));
 
@@ -35,17 +48,19 @@ public class Main {
 		graph[6].add(new Edge(6, 5, 3));
 
 		boolean[] visited = new boolean[7];
-		System.out.println(hasPath(graph, 2,4, visited));
+//		System.out.println(hasPath(graph, 2, 4, visited));
+		printAllPath(graph, 0, 6, visited, 0+"");
 	}
-	
+
 	public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] visited) {
-		if(src==dest) return true;
-		
+		if (src == dest)
+			return true;
+
 		visited[src] = true;
-		for(Edge edge: graph[src]) {
-			if(visited[edge.nbr] == false) {
+		for (Edge edge : graph[src]) {
+			if (visited[edge.nbr] == false) {
 				boolean nbrHasPath = hasPath(graph, edge.nbr, dest, visited);
-				if(nbrHasPath == true) {
+				if (nbrHasPath == true) {
 					return true;
 				}
 			}
@@ -53,4 +68,21 @@ public class Main {
 		return false;
 	}
 
+	public static void printAllPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] visited, String psf) {
+
+		if(src==dest) {
+			System.out.println(psf);
+			return;
+		}
+		
+		visited[src] = true;
+		
+		for(Edge edge: graph[src]) {
+			if(visited[edge.nbr]==false) {
+				printAllPath(graph, edge.nbr, dest, visited, psf+edge.nbr);
+			}
+		}
+		
+		visited[src] = false;
+	}
 }
